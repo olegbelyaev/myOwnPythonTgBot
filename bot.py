2d18e4f5313d825f31ab71b6373aff71a21fe2d5
olegbelyaev <vseh.pobedil@gmail.com>
2020-06-21 02:07:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,8 +8,8 @@ bot = telebot.TeleBot(cofig.token)
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
     bot.send_message(message.chat.id, message.text)
-    bot.send_message(cht, "version : 2.20")
-    bot.send_message(cht, "created by oleg ggwp gang")
+    bot.send_message(message.chat.id, "version : 2.20")
+    bot.send_message(message.chat.id, "created by oleg ggwp gang")
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)                     
