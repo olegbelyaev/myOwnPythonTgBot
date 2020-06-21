34fb3f43d2e40eb05f11b5fbf8e59e1e225355e7
olegbelyaev <vseh.pobedil@gmail.com>
2020-06-21 12:54:04 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,15 @@
-# -*- coding: utf-8 -*-
 import telebot
-token = '878350380:AAHqSNWtOXQvzESImpZmZSM5152r9YdypkE'
-bot = telebot.TeleBot(token)
-bot.send_message(500283434, "hey, it's version : 2.20")
+from telebot import types
+from telebot.types import Message
 
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, message.text)
-    bot.send_message(message.chat.id, "version : 2.20")
-    bot.send_message(message.chat.id, "created by oleg ggwp gang")
+TOKEN = '878350380:AAHqSNWtOXQvzESImpZmZSM5152r9YdypkE'
 
-bot.polling(none_stop=False, interval=0, timeout=20)                   
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def command_handler(message: Message):
+    bot.reply_to(message, 'There is no answer =(')
+
+
+bot.polling(timeout=60)
