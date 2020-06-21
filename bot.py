552de4eb9527ea85eb4aceaf0c43c47a16ff59e6
olegbelyaev <vseh.pobedil@gmail.com>
2020-06-21 10:13:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import cofig
 import telebot
 token = '878350380:AAHqSNWtOXQvzESImpZmZSM5152r9YdypkE'
 bot = telebot.TeleBot(token)
+bot.send_message(500283434, "hey, it's version : 2.20")
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
